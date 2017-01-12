14996c99465f21a47d39fad06cd84611587d6374
Arnaud Belcour <arnaud.belcour@gmail.com>
2017-01-12 19:40:51 +0100

    Add exceptions when retrieving GO number (from test with new data).

--- primaryFileManagement.py
+++ primaryFileManagement.py
@@ -1,4 +1,5 @@
 import pandas as pa
+import math
 from collections import defaultdict
 
 def GOLabelNumberDictionnaryCreation(fileName):
@@ -16,13 +17,13 @@ def GOLabelNumberDictionnaryCreation(fileName):
 
     queryResultsTable.columns = [["subject", "label", "NarrowSynonym", "BroadSynonym", "RelatedSynonym"]]
 
-    quoteDeletion = lambda x: str(x).replace('"', '')
+    quoteDeletion = lambda x: x.replace('"', '')
     queryResultsTable["subject"] = queryResultsTable["subject"].apply(quoteDeletion)
 
     goIsolation = lambda x: x[32:]
     queryResultsTable["subject"] = queryResultsTable["subject"].apply(goIsolation)
 
-    spaceDeletion = lambda x: str(x).replace(" ", "")
+    spaceDeletion = lambda x: x.replace(" ", "")
     queryResultsTable["label"] = queryResultsTable["label"].apply(spaceDeletion)
 
     spaceDeletion = lambda x: str(x).replace(" ", "")
@@ -35,7 +36,6 @@ def GOLabelNumberDictionnaryCreation(fileName):
     queryResultsTable["RelatedSynonym"] = queryResultsTable["RelatedSynonym"].apply(spaceDeletion)
 
     d_GOLabelToNumber = dict(zip(queryResultsTable['label'], queryResultsTable['subject']))
-
 
     d_GOLabelWithSynonym = {}
 
@@ -60,12 +60,16 @@ def GOLabelNumberDictionnaryCreation(fileName):
     for key in keysTodelete:
         del d_GOLabelWithSynonym[key]
 
-    d_GOLabelWithSynonym
-
     return d_GOLabelToNumber, d_GOLabelWithSynonym
 
 def translateGOTerm(listOfGOLabel, d_GOLabelToNumber):
     listOfGONumber = []
+
+    if type(listOfGOLabel) is float:
+        if math.isnan(listOfGOLabel):
+            listOfGOLabel = [str(listOfGOLabel)]
+
+    listOfGOLabel = [GO.replace(" ", "") for GO in listOfGOLabel]
 
     for GOLabel in listOfGOLabel:
         if GOLabel in d_GOLabelToNumber:
@@ -76,7 +80,7 @@ def translateGOTerm(listOfGOLabel, d_GOLabelToNumber):
 
     return listOfGONumber
 
-def fixObsoleteGOTerm(listOfGOLabelAndNumber, d_GOLabelToNumber):
+def fixObsoleteGOTerm(listOfGOLabelAndNumber, d_GOLabelToNumber, d_GOLabelWithSynonym):
     listOfGONumber = []
 
     for GOLabelAndNumber in listOfGOLabelAndNumber:
@@ -89,12 +93,15 @@ def fixObsoleteGOTerm(listOfGOLabelAndNumber, d_GOLabelToNumber):
             if GOLabelObsolete in d_GOLabelToNumber:
                 GONumber = d_GOLabelToNumber[GOLabelObsolete]
                 listOfGONumber.append(GONumber)
+            elif GOLabelObsolete in d_GOLabelWithSynonym:
+                GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                listOfGONumber.append(GONumber)
             else:
                 listOfGONumber.append(GOLabelAndNumber)
 
     return listOfGONumber
 
-def fixWrongNorLTerm(listOfGOLabelAndNumber, d_GOLabelToNumber):
+def fixWrongNorLTerm(listOfGOLabelAndNumber, d_GOLabelToNumber, d_GOLabelWithSynonym):
     listOfGONumber = []
 
     for GOLabelAndNumber in listOfGOLabelAndNumber:
@@ -108,13 +115,21 @@ def fixWrongNorLTerm(listOfGOLabelAndNumber, d_GOLabelToNumber):
                 if GOLabelNWrong in d_GOLabelToNumber:
                     GONumber = d_GOLabelToNumber[GOLabelNWrong]
                     listOfGONumber.append(GONumber)
+                elif GOLabelNWrong in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
                 else:
                     listOfGONumber.append(GOLabelAndNumber)
+
             elif "L" in GOLabelAndNumber :
                 GOLabelLWrong = GOLabelAndNumber.replace("L", "N")
                 if GOLabelLWrong in d_GOLabelToNumber:
                     GONumber = d_GOLabelToNumber[GOLabelLWrong]
                     listOfGONumber.append(GONumber)
+                elif GOLabelLWrong in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+
                 else:
                     listOfGONumber.append(GOLabelAndNumber)
             else:
@@ -139,7 +154,7 @@ def fixProblemsWithSynonym(listOfGOLabelAndNumber, d_GOLabelToNumber, d_GOLabelW
 
     return listOfGONumber
 
-def fixTermsIssue(listOfGOLabelAndNumber, d_GOLabelToNumber):
+def fixTermsIssue(listOfGOLabelAndNumber, d_GOLabelToNumber, d_GOLabelWithSynonym):
     listOfGONumber = []
 
     for GOLabelAndNumber in listOfGOLabelAndNumber:
@@ -153,20 +168,86 @@ def fixTermsIssue(listOfGOLabelAndNumber, d_GOLabelToNumber):
                 if GOLabelFixed in d_GOLabelToNumber:
                     GONumber = d_GOLabelToNumber[GOLabelFixed]
                     listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
             elif "al" in GOLabelAndNumber :
                 GOLabelFixed = GOLabelAndNumber.replace("al", "")
                 if GOLabelFixed in d_GOLabelToNumber:
                     GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
                     listOfGONumber.append(GONumber)
             elif "dependent" in GOLabelAndNumber :
                 GOLabelFixed = GOLabelAndNumber.replace("dependent", "templated")
                 if GOLabelFixed in d_GOLabelToNumber:
                     GONumber = d_GOLabelToNumber[GOLabelFixed]
                     listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
             elif "sequence-specific" in GOLabelAndNumber :
                 GOLabelFixed = GOLabelAndNumber.replace("sequence-specific", "") + ",sequence-specific"
                 if GOLabelFixed in d_GOLabelToNumber:
                     GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+            elif "homocysteineS-methyltransferaseactivity" in GOLabelAndNumber:
+                GOLabelFixed = "S-adenosylmethionine-" + GOLabelAndNumber
+                if GOLabelFixed in d_GOLabelToNumber:
+                    GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+            elif "Cul4-RINGubiquitinligasecomplex" in GOLabelAndNumber:
+                GOLabelFixed = GOLabelAndNumber[:9] + "E3" + GOLabelAndNumber[9:]
+                if GOLabelFixed in d_GOLabelToNumber:
+                    GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+            elif "organdevelopment" in GOLabelAndNumber:
+                GOLabelFixed = "animal" + GOLabelAndNumber
+                if GOLabelFixed in d_GOLabelToNumber:
+                    GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+            elif "mitosis" in GOLabelAndNumber:
+                GOLabelFixed = "mitoticnucleardivision" + GOLabelAndNumber
+                if GOLabelFixed in d_GOLabelToNumber:
+                    GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+            else:
+                listOfGONumber.append(GOLabelAndNumber)
+
+    return listOfGONumber
+
+def fixDashInExcess(listOfGOLabelAndNumber, d_GOLabelToNumber, d_GOLabelWithSynonym):
+    listOfGONumber = []
+
+    for GOLabelAndNumber in listOfGOLabelAndNumber:
+        if GOLabelAndNumber == "-":
+            listOfGONumber.append(GOLabelAndNumber)
+        elif GOLabelAndNumber[0:2] == "GO":
+            listOfGONumber.append(GOLabelAndNumber)
+        elif GOLabelAndNumber[0:2] != 'GO' and GOLabelAndNumber[0:2] != "-":
+            if "-" in GOLabelAndNumber :
+                GOLabelFixed = GOLabelAndNumber.replace("-", "")
+                if GOLabelFixed in d_GOLabelToNumber:
+                    GONumber = d_GOLabelToNumber[GOLabelFixed]
+                    listOfGONumber.append(GONumber)
+                elif GOLabelFixed in d_GOLabelWithSynonym:
+                    GONumber = d_GOLabelWithSynonym[GOLabelFixed]
                     listOfGONumber.append(GONumber)
             else:
                 listOfGONumber.append(GOLabelAndNumber)
@@ -175,7 +256,7 @@ def fixTermsIssue(listOfGOLabelAndNumber, d_GOLabelToNumber):
 
 def columnGOCleaning():
     resultsTable = pa.read_csv("Fichier3_Comparatif2ConditionsAvecAnnotations.csv", sep = ";")
-    resultsTable = resultsTable[['Nom_Gene', 'NAME_BROAD', 'NAME_Blast2Go', 'GOs', 'EnzymeCodes', 'InterProScan']]
+    resultsTable = resultsTable[['Row.names', 'SeqDescription', 'SeqLength', 'GOs', 'EnzymeCodes', 'InterProScan']]
 
     resultsTable['GOs'] = resultsTable['GOs'].str.replace("C:", "")
     resultsTable['GOs'] = resultsTable['GOs'].str.replace("P:", "")
@@ -184,22 +265,36 @@ def columnGOCleaning():
 
     d_GOLabelToNumber, d_GOLabelWithSynonym = GOLabelNumberDictionnaryCreation("queryResults.csv")
 
+    resultsTable = cleaningNanColumn(resultsTable, 'GOs')
+
     translation = lambda x: translateGOTerm(x, d_GOLabelToNumber)
     resultsTable['GOs'] = resultsTable['GOs'].apply(translation)
 
     correctionProblesmSynonym = lambda x : fixProblemsWithSynonym(x,  d_GOLabelToNumber, d_GOLabelWithSynonym)
     resultsTable['GOs'] = resultsTable['GOs'].apply(correctionProblesmSynonym)
 
-    correctionObsolete = lambda x: fixObsoleteGOTerm(x, d_GOLabelToNumber)
+    correctionObsolete = lambda x: fixObsoleteGOTerm(x, d_GOLabelToNumber, d_GOLabelWithSynonym)
     resultsTable['GOs'] = resultsTable['GOs'].apply(correctionObsolete)
 
-    correctionNorL = lambda x: fixWrongNorLTerm(x, d_GOLabelToNumber)
+    correctionNorL = lambda x: fixWrongNorLTerm(x, d_GOLabelToNumber, d_GOLabelWithSynonym)
     resultsTable['GOs'] = resultsTable['GOs'].apply(correctionNorL)
 
-    correctionTermsIssue = lambda x : fixTermsIssue(x, d_GOLabelToNumber)
+    correctionTermsIssue = lambda x : fixTermsIssue(x, d_GOLabelToNumber, d_GOLabelWithSynonym)
     resultsTable['GOs'] = resultsTable['GOs'].apply(correctionTermsIssue)
 
+    correctionDashIssue = lambda x : fixDashInExcess(x, d_GOLabelToNumber, d_GOLabelWithSynonym)
+    resultsTable['GOs'] = resultsTable['GOs'].apply(correctionDashIssue)
+
     return resultsTable
+
+def cleaningNanColumn(dataframe, column):
+
+    for index, row in dataframe.iterrows():
+        if type(index) is float:
+            if math.isnan(dataframe.get_value(index, column)):
+                dataframe = dataframe.drop([index])
+
+    return dataframe
 
 def rewritingFile(newtable, fileName):
     import csv
@@ -208,3 +303,5 @@ def rewritingFile(newtable, fileName):
 def main():
     resultsTable = columnGOCleaning()
     rewritingFile(resultsTable, "queryResultsGOTranslatedAndFixed.tsv")
+
+main()

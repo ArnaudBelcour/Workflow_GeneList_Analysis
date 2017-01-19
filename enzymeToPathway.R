189658a100652fa7928d1f6e30cf3c37047a03d8
arnaud belcour <arnaud.belcour@gmail.com>
2017-01-19 17:17:21 +0100

    Add a R script to convert enzyme code into pathway.

---
+++ enzymeToPathway.R
@@ -0,0 +1,32 @@
+library(KEGGREST)
+
+enzymeCodes = c('ec:3.6.1.3', 'ec:3.2.1.55', 'ec:3.2.1.78', 'ec:3.2.1.8', 'ec:2.7.3')
+
+enzymePathwayTable <- data.frame(matrix(ncol=3))
+colnames(enzymePathwayTable) <- c('ecCode', 'Pathway', 'PathwayID')
+enzymePathwayTable = enzymePathwayTable[-1,]
+
+write.table(enzymePathwayTable, file = "enzymeToPathway.tsv", append = FALSE, row.names = FALSE, col.names = TRUE, sep="\t")
+
+for(enzymeCode in enzymeCodes)
+{
+    pathwayLinkedToEnzyme = keggLink('pathway', enzymeCode)
+    if (toString(pathwayLinkedToEnzyme) == '')
+    {
+        enzymePathwayTable <- matrix(c(enzymeCode, NA, NA), ncol = 3)
+
+        write.table(enzymePathwayTable, file="enzymeToPathway.tsv", append = T, col.names = FALSE, row.names = FALSE, sep = "\t")
+    }
+
+    for (pathwayID in pathwayLinkedToEnzyme)
+    {
+        pathwayName = keggFind('pathway', pathwayID)
+
+        if (grepl(toString(pathwayName), "path:ec") == FALSE)
+        {
+            enzymePathwayTable <- matrix(c(enzymeCode, pathwayName, pathwayID), ncol = 3)
+
+            write.table(enzymePathwayTable, file="enzymeToPathway.tsv", append = TRUE, col.names = FALSE, row.names = FALSE, sep = "\t")
+        }
+    }
+}

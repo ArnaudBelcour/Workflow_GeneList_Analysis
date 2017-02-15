4cc4b40a4e4be33d5f61b4f318b94d51d37fd369
arnaud belcour <arnaud.belcour@gmail.com>
2017-02-15 18:14:40 +0100

    Update pathway extraction from Reactome.

--- pathway-extraction/pathway_reactome.py
+++ pathway-extraction/pathway_reactome.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python3
 
+import csv
 import requests
 
-def http_request_reactome(code):
+def http_request_reactome(code, writer):
     try:
         r = requests.get('http://www.reactome.org/ContentService/search/query?query=' + code +'&cluster=true')
         results = r.json()
-        result_id = results['results'][0]['entries'][0]['stId']
+        print(results)
 
-        r = requests.get('http://www.reactome.org/ContentService/data/pathways/low/entity/' + result_id)
-        print(r.json())
-    except:
-        print(r.json()['messages'][0])
+        if 'code' in results:
+            if results['code'] == 404:
+                return print('No results found')
 
-    return r.text
+        #r = requests.get('http://www.reactome.org/ContentService/data/pathways/low/entity/' + result_id)
+        if "GO" in code:
+            for index in range(len(results['results'][0]['entries'])):
+                reactome_id = results['results'][0]['entries'][index]['id']
+                reactome_id_specie = results['results'][0]['entries'][index]['species']
+                writer.writerow([code, reactome_id, reactome_id_specie])
+        if "." in code:
+            for index in range(len(results['results'][0]['entries'])):
+                reactome_id = results['results'][0]['entries'][index]['id']
+                reactome_id_specie =results['results'][0]['entries'][index]['species']
+                writer.writerow([code, reactome_id, reactome_id_specie])
+        if "REACT" in code:
+            for index in range(len(results['results'][0]['entries'])):
+                reactome_id = results['results'][0]['entries'][index]['id']
+                reactome_id_specie = results['results'][0]['entries'][index]['species']
+                writer.writerow([code, reactome_id, reactome_id_specie])
+        if "CHEBI" in code:
+            for index in range(len(results['results'])):
+                for index2 in range(len(results['results'][index]['entries'])):
+                    reactome_id = results['results'][index]['entries'][index2]['id']
+                    reactome_id_specie = results['results'][index]['entries'][index2]['species']
+                    writer.writerow([code, reactome_id, reactome_id_specie])
+
+    except Exception as e:
+        print ("Errors : " + repr(e))
 
 def main():
-    test = http_request_reactome('0007264')
+    csvfile = open('temporaryFiles/databases/enzyme_pathway_reactome.tsv', "w", newline = "")
+    writer = csv.writer(csvfile, delimiter="\t")
+    writer.writerow(['code', 'Id', 'specie'])
+    http_request_reactome('CHEBI:18139', writer)
+    #for ec in ecs_requests:
+        #ec = ec.strip()[len('ec:'):]
+        #test = http_request_reactome(ec, writer)
+    csvfile.close()
 
 main()

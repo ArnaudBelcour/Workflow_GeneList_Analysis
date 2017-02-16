5b69dd3815331a02249f379e859f878450e86ed2
Arnaud Belcour <arnaud.belcour@gmail.com>
2017-02-16 10:39:17 +0100

    Add a function translating GO terms in ChEBI.

--- pathway_extraction.py
+++ pathway_extraction.py
@@ -41,6 +41,19 @@ def r_keggrest_ec(ecs_requests):
     except subprocess.CalledProcessError as e:
         raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
+        temporary_directory_database = "temporaryFiles/databases/"
+
+def translation_go_chebi(selected_gos):
+    chebis = []
+    for selected_go in literal_eval(selected_gos):
+        if selected_go in go_to_chebi.index:
+            if type(go_to_chebi.loc[selected_go]['ChEBI']) == str :
+                chebis.append(go_to_chebi.loc[selected_go]['ChEBI'])
+            if type(go_to_chebi.loc[selected_go]['ChEBI']) == list :
+                chebis.extend(go_to_chebi.loc[selected_go]['ChEBI'].tolist())
+
+    return set(chebis)
+
 def main():
     if os.path.exists(temporary_directory_database[:-1]) == False :
         os.makedirs(temporary_directory_database)
@@ -49,4 +62,7 @@ def main():
     ecs_requests = ec_extraction(df_genome)
     r_keggrest_ec(ecs_requests)
 
+    go_to_chebi = pa.read_csv(temporary_directory_database + "go_chebi_mapping.tsv", sep = '\t')
+    go_to_chebi = go_to_chebi.set_index("GOs")
+    df_genome['ChEBI'] = df_genome['GOs'].apply(translation_go_chebi)
 main()

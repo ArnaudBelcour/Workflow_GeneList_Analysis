1beffa33d61bf5c0ff45627ef631f5fb6d7cc7ae
arnaud belcour <arnaud.belcour@gmail.com>
2017-02-21 12:02:00 +0100

    Rename pathway_extraction folder.

--- pathway_extraction/uniprot_retrieval_data.py
+++ pathway_extraction/uniprot_retrieval_data.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 
+import math
 import pandas as pa
 from ast import literal_eval
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-input_directory = "inputFiles/"
-temporary_directory = 'temporaryFiles/'
-output_directory = 'outputFiles/'
+input_directory = "../inputFiles/"
+temporary_directory = '../temporaryFiles/'
+output_directory = '../outputFiles/'
 
-def extract_information_from_uniprot():
+def extract_information_from_uniprot(file_name):
     '''
         Requests the SPARQL endpoint of Uniprot to retrieve (from Ensembl transcrit ID) GO terms, interpro, pfam/supfam and prosites.
         The file taken as input file contains each gene associated with the result of a blast (that's the thing with 'hypothetical protein').
     '''
-    df_genome = pa.read_excel('inputFiles/Annotation_blast2go_PROT_eH.xls', sep = "\t")
+    df_genome = pa.read_excel(input_directory + file_name, sep = "\t")
     df_genome['Blast_sur_e3'] = df_genome['Blast_sur_e3'].str[len('CEP03957.1hypothetical protein '):]
     df_genome['Blast_sur_e3'] = df_genome['Blast_sur_e3'].str.replace(", partial", "")
-    df_genome = df_genome[['SeqName_eH', 'Blast_sur_e3']]
+
     df_genome.set_index("SeqName_eH")
-    df_genome = df_genome[:50]
+
     for gene, row in df_genome.iterrows():
         transcript = 'ensembl:' + row['Blast_sur_e3']
         gos_found = []
@@ -63,6 +64,7 @@ def extract_information_from_uniprot():
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
         datas_found = []
+        print(results["results"]["bindings"])
         for result in results["results"]["bindings"]:
             datas_found.append(result["data"]["value"][len('http://purl.uniprot.org/'):])
 
@@ -85,11 +87,14 @@ def extract_information_from_uniprot():
                 data = data[len('prosite/'):]
                 prosites.append(data)
 
-        df_genome.set_value(gene, 'Interpros', str(interpros))
-        df_genome.set_value(gene, 'GOs', str(gos_found))
+        if type(row['Gos']) is float:
+            df_genome.set_value(gene, 'Gos', str(gos_found))
+        if row['InterPro IDs'] == 'no IPS match':
+            df_genome.set_value(gene, 'InterPro IDs', str(interpros))
+
         df_genome.set_value(gene, 'supFams', str(supfams))
         df_genome.set_value(gene, 'pfams', str(pfams))
         df_genome.set_value(gene, 'prosites', str(prosites))
 
-    df_genome.to_csv(temporary_directory + 'annotations_from_uniprot.tsv', sep = "\t")
-extract_information_from_uniprot()
+    df_genome.to_csv(temporary_directory + file_name[:-4] + '.tsv', sep = "\t")
+extract_information_from_uniprot('Annotation_blast2go_PROT_eH.xls')

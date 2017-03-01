678dee999b21569168121df7294af1e63f27a7cf
ArnaudBelcour <arnaud.belcour@gmail.com>
2017-03-01 09:25:13 +0100

    Update enrichmentAnalysis_test.py

--- test/enrichmentAnalysis_test.py
+++ test/enrichmentAnalysis_test.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pa
 import scipy.stats as stats
-import sys
 import unittest
 
-sys.path.append("..")
-
-from enrichmentAnalysis import EnrichmentAnalysis
+from ..enrichmentAnalysis import EnrichmentAnalysis
 
 test_data_directory = '../test_data/'
 test_data_directory_cdf = test_data_directory + 'test_cdf/'

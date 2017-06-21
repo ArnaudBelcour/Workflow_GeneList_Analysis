12f732de49b9917aa8e9f81f272dc16e383664bc
Arnaud Belcour <arnaud.belcour@gmail.com>
2017-06-21 11:30:14 +0200

    Add liasis in requirements.txt. Fix an issue with uniprot test.

--- test/test_uniprot_retrieval_data.py
+++ test/test_uniprot_retrieval_data.py
@@ -17,4 +17,4 @@ class uniprot_retrieval_data_test(unittest.TestCase):
         df_result_truth = pa.read_csv(test_data_directory_uniprot + 'result.tsv', sep='\t')
 
         np.testing.assert_array_equal(df_result['GOs'].tolist().sort(), df_result_truth['GOs'].tolist().sort())
-        np.testing.assert_array_equal(df_result['InterProScan'].tolist(), df_result_truth['InterProScan'].tolist())
+        np.testing.assert_array_equal(df_result['InterProScan'].tolist().sort(), df_result_truth['InterProScan'].tolist().sort())
